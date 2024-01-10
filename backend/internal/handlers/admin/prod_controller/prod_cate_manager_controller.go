1647e58d9e25fcc915db1b0de29eaeffa78f9f3b
wulinlin <18574945291@163.com>
2024-01-10 09:35:34 +0800

    init

---
+++ backend/internal/handlers/admin/prod_controller/prod_cate_manager_controller.go
@@ -0,0 +1,7 @@
+package prod_controller
+
+//
+// @Description
+// @Author 代码小学生王木木
+// @Date 2024/1/10 9:30
+//
---
+++ backend/internal/handlers/admin/shop_controller/controller.go
@@ -0,0 +1,7 @@
+package shop_controller
+
+//
+// @Description
+// @Author 代码小学生王木木
+// @Date 2024/1/10 9:31
+//
